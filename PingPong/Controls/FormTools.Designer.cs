36c23cc9d1e26f605f9e9cbb3b419808ad7bb718
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-28 16:33:39 +0330

    - Create Icon Button In FormTools Control

--- PingPong/Controls/FormTools.Designer.cs
+++ PingPong/Controls/FormTools.Designer.cs
@@ -28,10 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnIcon = new System.Windows.Forms.Button();
+            this.btnMinimize = new System.Windows.Forms.Button();
             this.btnMaxRes = new System.Windows.Forms.Button();
             this.btnClose = new System.Windows.Forms.Button();
-            this.btnMinimize = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnIcon
+            // 
+            this.btnIcon.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.btnIcon.BackgroundImage = global::PingPong.Properties.Resources.PingPongIcon;
+            this.btnIcon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnIcon.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIcon.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnIcon.FlatAppearance.BorderSize = 0;
+            this.btnIcon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIcon.Location = new System.Drawing.Point(0, 0);
+            this.btnIcon.Name = "btnIcon";
+            this.btnIcon.Size = new System.Drawing.Size(45, 45);
+            this.btnIcon.TabIndex = 1;
+            this.btnIcon.UseVisualStyleBackColor = false;
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.BackColor = System.Drawing.Color.Transparent;
+            this.btnMinimize.BackgroundImage = global::PingPong.Properties.Resources.MinimizeButton;
+            this.btnMinimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnMinimize.FlatAppearance.BorderSize = 0;
+            this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimize.Font = new System.Drawing.Font("Segoe MDL2 Assets", 2F);
+            this.btnMinimize.ForeColor = System.Drawing.Color.White;
+            this.btnMinimize.Location = new System.Drawing.Point(439, 2);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(44, 40);
+            this.btnMinimize.TabIndex = 0;
+            this.btnMinimize.UseVisualStyleBackColor = false;
             // 
             // btnMaxRes
             // 
@@ -63,26 +94,12 @@
             this.btnClose.TabIndex = 0;
             this.btnClose.UseVisualStyleBackColor = false;
             // 
-            // btnMinimize
-            // 
-            this.btnMinimize.BackColor = System.Drawing.Color.Transparent;
-            this.btnMinimize.BackgroundImage = global::PingPong.Properties.Resources.MinimizeButton;
-            this.btnMinimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnMinimize.FlatAppearance.BorderSize = 0;
-            this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnMinimize.Font = new System.Drawing.Font("Segoe MDL2 Assets", 2F);
-            this.btnMinimize.ForeColor = System.Drawing.Color.White;
-            this.btnMinimize.Location = new System.Drawing.Point(439, 2);
-            this.btnMinimize.Name = "btnMinimize";
-            this.btnMinimize.Size = new System.Drawing.Size(44, 40);
-            this.btnMinimize.TabIndex = 0;
-            this.btnMinimize.UseVisualStyleBackColor = false;
-            // 
             // FormTools
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.Controls.Add(this.btnIcon);
             this.Controls.Add(this.btnMinimize);
             this.Controls.Add(this.btnMaxRes);
             this.Controls.Add(this.btnClose);
@@ -97,5 +114,6 @@
         private System.Windows.Forms.Button btnClose;
         private System.Windows.Forms.Button btnMaxRes;
         private System.Windows.Forms.Button btnMinimize;
+        private System.Windows.Forms.Button btnIcon;
     }
 }
--- PingPong/Properties/Resources.Designer.cs
+++ PingPong/Properties/Resources.Designer.cs
@@ -89,5 +89,15 @@ namespace PingPong.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap PingPongIcon {
+            get {
+                object obj = ResourceManager.GetObject("PingPongIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
