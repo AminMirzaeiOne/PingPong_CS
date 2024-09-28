dd4ebaa299f56e3dc2788721d0dbe265835cc061
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-28 16:17:13 +0330

    - Create Close Button In FormTools Control

--- PingPong/Controls/FormTools.Designer.cs
+++ PingPong/Controls/FormTools.Designer.cs
@@ -28,13 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnClose = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Transparent;
+            this.btnClose.BackgroundImage = global::PingPong.Properties.Resources.CloseButton;
+            this.btnClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Segoe MDL2 Assets", 2F);
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(552, 2);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(44, 40);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.UseVisualStyleBackColor = false;
             // 
             // FormTools
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.Controls.Add(this.btnClose);
             this.Name = "FormTools";
             this.Size = new System.Drawing.Size(600, 45);
             this.ResumeLayout(false);
@@ -42,5 +59,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnClose;
     }
 }
