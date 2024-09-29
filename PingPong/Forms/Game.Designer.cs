ce7400c37ac33959ade5c8c433b3beb2f3c8f67c
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-29 22:53:06 +0330

    - Create Show Points Panel In Game Form

--- PingPong/Forms/Game.Designer.cs
+++ PingPong/Forms/Game.Designer.cs
@@ -31,8 +31,19 @@
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
             this.formTools1 = new PingPong.Controls.FormTools();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblCPU = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lblCpuPoints = new System.Windows.Forms.Label();
+            this.lblCpuColor = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -52,6 +63,8 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.formTools1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(3, 3);
@@ -69,6 +82,109 @@
             this.formTools1.TabIndex = 0;
             this.formTools1.Window = this;
             // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.panel2.Location = new System.Drawing.Point(260, 81);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 100);
+            this.panel2.TabIndex = 1;
+            // 
+            // lblCPU
+            // 
+            this.lblCPU.AutoSize = true;
+            this.lblCPU.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCPU.ForeColor = System.Drawing.Color.White;
+            this.lblCPU.Location = new System.Drawing.Point(79, 15);
+            this.lblCPU.Name = "lblCPU";
+            this.lblCPU.Size = new System.Drawing.Size(128, 30);
+            this.lblCPU.TabIndex = 0;
+            this.lblCPU.Text = "CPU Points :";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.lblCpuColor);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.lblCpuPoints);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.lblCPU);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 534);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(594, 60);
+            this.panel3.TabIndex = 2;
+            // 
+            // lblCpuPoints
+            // 
+            this.lblCpuPoints.AutoSize = true;
+            this.lblCpuPoints.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCpuPoints.ForeColor = System.Drawing.Color.White;
+            this.lblCpuPoints.Location = new System.Drawing.Point(208, 15);
+            this.lblCpuPoints.Name = "lblCpuPoints";
+            this.lblCpuPoints.Size = new System.Drawing.Size(25, 30);
+            this.lblCpuPoints.TabIndex = 0;
+            this.lblCpuPoints.Text = "3";
+            // 
+            // lblCpuColor
+            // 
+            this.lblCpuColor.BackColor = System.Drawing.Color.Crimson;
+            this.lblCpuColor.Location = new System.Drawing.Point(53, 21);
+            this.lblCpuColor.Name = "lblCpuColor";
+            this.lblCpuColor.Size = new System.Drawing.Size(20, 20);
+            this.lblCpuColor.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(368, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Player Points :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(515, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 30);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "10";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.RoyalBlue;
+            this.label3.Location = new System.Drawing.Point(342, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 20);
+            this.label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.RoyalBlue;
+            this.label4.Location = new System.Drawing.Point(289, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(2, 61);
+            this.label4.TabIndex = 3;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.RoyalBlue;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label5.Location = new System.Drawing.Point(0, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(594, 2);
+            this.label5.TabIndex = 4;
+            // 
             // Game
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
@@ -82,6 +198,8 @@
             this.Text = "Game";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -91,5 +209,15 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Panel panel1;
         private Controls.FormTools formTools1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lblCpuPoints;
+        private System.Windows.Forms.Label lblCPU;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblCpuColor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
--- PingPong/Forms/Game.cs
+++ PingPong/Forms/Game.cs
@@ -16,5 +16,10 @@ namespace PingPong.Forms
         {
             InitializeComponent();
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
