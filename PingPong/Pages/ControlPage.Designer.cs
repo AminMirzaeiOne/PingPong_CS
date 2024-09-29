c03b59b5396010e4548d69bb27120ef54e1eff22
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-29 15:25:43 +0330

    - Create Maximize and Restore Button In Control Page

--- PingPong/Pages/ControlPage.Designer.cs
+++ PingPong/Pages/ControlPage.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.btnClose = new System.Windows.Forms.Button();
+            this.btnMaxRes = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnClose
@@ -38,18 +39,33 @@
             this.btnClose.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnClose.ForeColor = System.Drawing.Color.White;
             this.btnClose.Image = global::PingPong.Properties.Resources.CloseButton;
-            this.btnClose.Location = new System.Drawing.Point(24, 15);
+            this.btnClose.Location = new System.Drawing.Point(24, 22);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(200, 60);
             this.btnClose.TabIndex = 0;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = true;
             // 
+            // btnMaxRes
+            // 
+            this.btnMaxRes.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMaxRes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMaxRes.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMaxRes.ForeColor = System.Drawing.Color.White;
+            this.btnMaxRes.Image = global::PingPong.Properties.Resources.MaxResButton;
+            this.btnMaxRes.Location = new System.Drawing.Point(24, 104);
+            this.btnMaxRes.Name = "btnMaxRes";
+            this.btnMaxRes.Size = new System.Drawing.Size(200, 60);
+            this.btnMaxRes.TabIndex = 0;
+            this.btnMaxRes.Text = "Maximize";
+            this.btnMaxRes.UseVisualStyleBackColor = true;
+            // 
             // ControlPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.Controls.Add(this.btnMaxRes);
             this.Controls.Add(this.btnClose);
             this.Name = "ControlPage";
             this.Size = new System.Drawing.Size(246, 396);
@@ -60,5 +76,6 @@
         #endregion
 
         private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnMaxRes;
     }
 }
