6150a6e709306de7ad70148d71bd1d5729400502
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-30 06:06:54 +0330

    - Add Click Action Name Button In Developer Page

--- PingPong/Pages/DeveloperPage.Designer.cs
+++ PingPong/Pages/DeveloperPage.Designer.cs
@@ -29,24 +29,53 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::PingPong.Properties.Resources.AminMirzaei;
-            this.pictureBox1.Location = new System.Drawing.Point(103, 19);
+            this.pictureBox1.Location = new System.Drawing.Point(118, 19);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(150, 150);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.button1.Location = new System.Drawing.Point(0, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(394, 66);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Name : Amin Mirzaei";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.button2.Location = new System.Drawing.Point(0, 280);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(394, 66);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Github : AminMirzaeiOne";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // DeveloperPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "DeveloperPage";
             this.Size = new System.Drawing.Size(394, 387);
@@ -58,5 +87,7 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- PingPong/Pages/DeveloperPage.cs
+++ PingPong/Pages/DeveloperPage.cs
@@ -16,5 +16,10 @@ namespace PingPong.Pages
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Clipboard.SetText(this.button1.Text);
+        }
     }
 }
