dbc758e9c53fc7e70483be149fe3655b3c9638af
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-30 16:17:20 +0330

    - Update Backend Feedback Form

--- PingPong/Forms/Feedback.Designer.cs
+++ PingPong/Forms/Feedback.Designer.cs
@@ -33,7 +33,11 @@
             this.btnSend = new System.Windows.Forms.Button();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -41,9 +45,9 @@
             this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(12, 93);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(504, 410);
+            this.groupBox1.Size = new System.Drawing.Size(504, 329);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Feedback Text";
@@ -58,7 +62,7 @@
             this.textBox1.Location = new System.Drawing.Point(3, 33);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(498, 374);
+            this.textBox1.Size = new System.Drawing.Size(498, 293);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "Enter Feedback Text";
             // 
@@ -74,6 +78,7 @@
             this.btnSend.TabIndex = 1;
             this.btnSend.Text = "Send";
             this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
             // 
             // radioButton1
             // 
@@ -101,12 +106,50 @@
             this.radioButton2.Text = "Report";
             this.radioButton2.UseVisualStyleBackColor = true;
             // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(504, 75);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Email Info";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.textBox2.ForeColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(16, 35);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(229, 31);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "From ...";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Black;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.textBox3.ForeColor = System.Drawing.Color.White;
+            this.textBox3.Location = new System.Drawing.Point(269, 33);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(229, 31);
+            this.textBox3.TabIndex = 1;
+            this.textBox3.Text = "Password ...";
+            // 
             // Feedback
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
             this.ClientSize = new System.Drawing.Size(528, 494);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.btnSend);
@@ -117,6 +160,8 @@
             this.Text = "Feedback";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -129,5 +174,8 @@
         private System.Windows.Forms.Button btnSend;
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
--- PingPong/Forms/Feedback.cs
+++ PingPong/Forms/Feedback.cs
@@ -7,14 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
 
 namespace PingPong.Forms
 {
     public partial class Feedback : Form
     {
+        SmtpClient client = new SmtpClient();
         public Feedback()
         {
             InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential(textBox2.Text.Trim(), textBox3.Text.Trim());
+
+            MailAddress from = new MailAddress(textBox2.Text);
+            MailAddress to = new MailAddress("aminmirzaei.og@gmail.com");
+            MailMessage message = new MailMessage(from, to);
+            message.Body = textBox1.Text;
+            message.BodyEncoding = Encoding.UTF8;
+            if (this.radioButton1.Checked)
+                message.Subject = "Feedback";
+            else
+                message.Subject = "Report";
+
+            message.SubjectEncoding = Encoding.UTF8;
+            client.Send(message);
+            message.Dispose();
+            MessageBox.Show("E-Mail Sent!");
         }
     }
 }
