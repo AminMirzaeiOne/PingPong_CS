94a2c582928a13463d020fd554fb0b95c69fdc83
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-30 05:55:43 +0330

    - Create Version Page In Pages Folder

---
+++ PingPong/Pages/VersionPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPong.Pages
+{
+    public partial class VersionPage : UserControl
+    {
+        public VersionPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
